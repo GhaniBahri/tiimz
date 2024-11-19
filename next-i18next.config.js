8d8f18145a0306770896df49c167128a0542d108
abdelghani bahri <bahri.ghanou@gmail.com>
2024-11-19 22:11:06 +0100

    thrut and lie input UI

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -8,7 +8,7 @@ module.exports = {
   localePath: path.resolve("./public/locales"),
   fallbackLng: 'en',
   // ns: ['common'],
-  debug: process.env.NODE_ENV === 'development',
+  // debug: process.env.NODE_ENV === 'development',
   defaultNS: 'common',
   // localePath: './public/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
--- src/pages/ThrutLie/index.tsx
+++ src/pages/ThrutLie/index.tsx
@@ -5,25 +5,31 @@ import { useTranslation } from 'next-i18next'
 import {auth, db} from '@/utils/firebase'
 import { signInAnonymously } from 'firebase/auth'
 import {doc, setDoc} from 'firebase/firestore'
+import { useRouter } from 'next/router'
+
 
 function Index() {
     const [nameInput, setNameInput]= useState("")
+    const [nameBox, setNameBox]= useState(false)
     const {t}= useTranslation(("common"))
+    const router = useRouter()
+    // console.log('query', router.query)
 
     const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) =>{
         setNameInput(e.target.value )
     }
 
     const handleSubmit = () => {
-        console.log('submitiing')
         signInAnonymously(auth)
         .then((data) => {
           // Signed in..
-          console.log('data signed in', data)
+        //   console.log('data signed in', data)
           setDoc(doc(db, "players", data.user.uid),{
-            name: nameInput
+            name: nameInput,
+            session: router.query.act,
+            createdAt: new Date().toString()
           }).then(res=>{
-            console.log("data added", res)
+            // console.log("data added", res)
             setNameInput('')
           }).catch(error=> console.error('error creating collection', error))
          
@@ -36,7 +42,7 @@ function Index() {
     <>
         <main className='relative bg-white'>
             <div className='fixed top-0 right-0 bottom-0 left-0'>
-                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 md:w-1/2 h-fit border-4 border-DodgerBlue dark:border-DodgerBlue rounded-xl
+                {nameBox ? (<div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 md:w-1/2 h-fit border-4 border-DodgerBlue dark:border-GoldenYellow rounded-xl
                         flex flex-col justify-end items-center gap-8 px-20 py-10'>
                     <label htmlFor="nameInput" className='w-full text-left text-Coralred text-3xl font-medium'>{t('enter name')}</label>
                     <input value={nameInput} type="text" name='nameInput' id='nameInput' onChange={handleNameInput}
@@ -46,6 +52,25 @@ function Index() {
                         {t("submit")}
                     </button>
                 </div>
+                )
+            :
+            (
+                <div className='w-full h-full flex flex-col justify-center items-center'>
+                    <div className='border-4 border-DodgerBlue dark:border-GoldenYellow rounded-xl relative flex justify-between items-center w-11/12 md:w-1/2 h-fit mb-12 px-20 py-10 flex-col gap-10'>
+                        <label htmlFor="lie" className='absolute bg-white left-6 -top-7 font-semibold text-xl text-TextPrimaryLight px-4 py-2 flex justify-center items-center '>2 Thruts</label>
+                        <input type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
+                        <input type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
+                    </div>
+                    <div className='border-4 border-DodgerBlue dark:border-GoldenYellow rounded-xl relative flex justify-between items-center w-11/12 md:w-1/2 h-fit mb-12 px-20 py-10'>
+                        <label htmlFor="lie" className='absolute bg-white left-6 -top-7 font-semibold text-xl text-TextPrimaryLight px-4 py-2 flex justify-center items-center '>1 Lie</label>
+                        <input type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
+                    </div>
+                    <button onClick={handleSubmit}
+                        className='h-16  w-fit px-8 rounded-lg bg-Coralred text-white text-2xl text-left font-medium outline-none'>
+                        {t("submit")}
+                    </button>
+                </div>
+            )}
             </div>
         </main>
     </>
