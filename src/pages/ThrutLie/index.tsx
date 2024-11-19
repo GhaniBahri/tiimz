74ec2aa5c0dfddf01382de55bb196b852b26a4c4
abdelghani bahri <bahri.ghanou@gmail.com>
2024-11-19 22:32:34 +0100

    submit facts

--- src/pages/ThrutLie/index.tsx
+++ src/pages/ThrutLie/index.tsx
@@ -10,6 +10,7 @@ import { useRouter } from 'next/router'
 
 function Index() {
     const [nameInput, setNameInput]= useState("")
+    const [facts, setFacts]= useState({})
     const [nameBox, setNameBox]= useState(false)
     const {t}= useTranslation(("common"))
     const router = useRouter()
@@ -18,7 +19,12 @@ function Index() {
     const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) =>{
         setNameInput(e.target.value )
     }
-
+    const handleFacts = (e: React.ChangeEvent<HTMLInputElement>)=>{
+        // console.log('facts', e)
+        setFacts({[e.target.name]: e.target.value, ...facts})
+        console.log('facts', facts)
+    }
+    const submitFacts= ()=>{}
     const handleSubmit = () => {
         signInAnonymously(auth)
         .then((data) => {
@@ -58,14 +64,14 @@ function Index() {
                 <div className='w-full h-full flex flex-col justify-center items-center'>
                     <div className='border-4 border-DodgerBlue dark:border-GoldenYellow rounded-xl relative flex justify-between items-center w-11/12 md:w-1/2 h-fit mb-12 px-20 py-10 flex-col gap-10'>
                         <label htmlFor="lie" className='absolute bg-white left-6 -top-7 font-semibold text-xl text-TextPrimaryLight px-4 py-2 flex justify-center items-center '>2 Thruts</label>
-                        <input type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
-                        <input type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
+                        <input onChange={handleFacts} name="thrut1" id='thrut1' type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
+                        <input onChange={handleFacts} name="thrut2" id='thrut2' type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
                     </div>
                     <div className='border-4 border-DodgerBlue dark:border-GoldenYellow rounded-xl relative flex justify-between items-center w-11/12 md:w-1/2 h-fit mb-12 px-20 py-10'>
                         <label htmlFor="lie" className='absolute bg-white left-6 -top-7 font-semibold text-xl text-TextPrimaryLight px-4 py-2 flex justify-center items-center '>1 Lie</label>
-                        <input type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
+                        <input onChange={handleFacts} name="lie" id='lie' type="text" className='shadow-[0_0_8px_2px_rgba(0,0,0,0.15)] bg-gray-50 rounded-sm w-full h-16 outline-none border-b-2 border-Coralred text-Coralred px-3 text-3xl text-left font-medium' />
                     </div>
-                    <button onClick={handleSubmit}
+                    <button onClick={submitFacts}
                         className='h-16  w-fit px-8 rounded-lg bg-Coralred text-white text-2xl text-left font-medium outline-none'>
                         {t("submit")}
                     </button>
